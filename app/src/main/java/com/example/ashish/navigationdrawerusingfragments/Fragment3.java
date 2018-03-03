848a7cf52a7c55bcd80d9515d71a60e797fe97cb
Ashish Rawat <ashishrawat2911@gmail.com>
2018-03-03 09:58:11 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ashish/navigationdrawerusingfragments/Fragment3.java
@@ -0,0 +1,53 @@
+package com.example.ashish.navigationdrawerusingfragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+/**
+ * Created by Ashish on 3/3/2018.
+ */
+
+public class Fragment3 extends Fragment {
+    ImageView imageView;
+
+    public Fragment3() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_fragment, container, false);
+        imageView=v.findViewById(R.id.imageView);
+        return v;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //you can set the title for your toolbar here for different fragments different titles
+        getActivity().setTitle("Menu 3");
+        imageView.setImageResource(R.drawable.helloworld3);
+
+    }
+
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+
+}
